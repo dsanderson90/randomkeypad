c49246141748eb1a597552d10978961447701973
dsanderson90 <dsanderson3@cnm.edu>
2019-06-14 12:39:11 -0600

    cleaned up if statements and made it so only numbers keys are allowed to be entered as pin

--- index.js
+++ index.js
@@ -1,60 +1,51 @@
 const keys = ['1', '2', '3' , '4', '5', '6', '7', '8', '9', '*', '0', '#'];
-
 const numpad = document.querySelector('.numpad');
 const screen =  document.getElementById('screen')
 const createPinBtn = document.getElementById('create');
 const div = document.getElementById("div");
 
 let pin = null;
-
 let correct = 'CORRECT'
 let incorrect = 'INCORRECT'
 let pinreq = 'Pin must be 4 digits'
 
 function shuffle(array) {
   var m = array.length, t, i;
-
   // While there remain elements to shuffle…
   while (m) {
-
     // Pick a remaining element…
     i = Math.floor(Math.random() * m--);
-
     // And swap it with the current element.
     t = array[m];
     array[m] = array[i];
     array[i] = t;
   }
-
   return array;
 }
 
 function populateButtons() {
-//  create a copy of the keys array
-let newKeys = [...keys];
+  //  create a copy of the keys array
+  let newKeys = [...keys];
 
-//  shuffle the new array
-shuffle(newKeys);
+  //  shuffle the new array
+  shuffle(newKeys);
 
-//  loop through new array and make a button for each element
-for (var i = 0; i < newKeys.length; i++) {
-let key = document.createElement('button');
-key.className = 'key'
-key.innerText= newKeys[i];
+  //  loop through new array and make a button for each element
+  for (var i = 0; i < newKeys.length; i++) {
+  let key = document.createElement('button');
+  key.className = 'key'
+  key.innerText= newKeys[i];
 
-//  add an event listener to each btn that will add its value to the display
-key.addEventListener('click', function() {
+  //  add an event listener to each btn that will add its value to the display
+  key.addEventListener('click', function() {
 
   screen.value += key.innerText;
-  if (screen.value.length > 4) {
-    screen.value = '';
-  }
-  else if (screen.value.length == 4 && screen.value != pin && pin != null) {
-    screen.value = incorrect
-  }
-
-
-
+    if (screen.value.length > 4) {
+      screen.value = '';
+    }
+    else if (screen.value.length == 4 && screen.value != pin && pin != null) {
+      screen.value = incorrect
+    }
 });
 numpad.appendChild(key);
   }
@@ -65,7 +56,7 @@ function removeButtons() {
   //  remove the keys but keep the display input and randomize btn
   while (div.childNodes.length > 6) {
     div.removeChild(div.lastChild);
-}
+  }
 }
 // populate buttons on page load
     populateButtons();
@@ -78,12 +69,15 @@ randomBtn.addEventListener('click', function() {
 })
 
 function createPin() {
-
+  //the pin must be 4 digits
    if (screen.value.length < 4) {
     screen.value = pinreq
   }
   else if (screen.value.length === 4) {
     pin = screen.value;
+  }
+  else {
+    screen.value = pinreq
   }
 }
 
@@ -93,9 +87,13 @@ function clearScreen() {
 
 function verifyPin() {
   if (screen.value == pin) {
-  screen.value = correct
-  document.body.style.backgroundColor = 'lime';
-}
+    screen.value = correct
+    document.body.style.backgroundColor = 'lime';
+  }
+  else {
+    screen.value = incorrect
+    document.body.style.backgroundColor = 'red';
+  }
 
 }
 
@@ -132,38 +130,28 @@ function replaceBtn() {
 
 // keyboard events
 document.onkeydown = function (e) {
-
-  if((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
-        screen.value += e.key
+  // if the key is a number add it to the screen
+  // special chars and letters are not allowed.
+  let key = Number(e.key)
+  if(isNaN(key) == false) {
+    screen.value += e.key
   }
-
+  // backspace handling
   if(e.keyCode == 8 && screen.value != incorrect && screen.value != pinreq) {
+    // remove one letter from the end of the string when backspace is hit.
     screen.value = screen.value.substring(0, screen.value.length-1)
   }
-
   if(e.keyCode == 8 && screen.value == incorrect|| screen.value == pinreq || screen.value == correct) {
-
     clearScreen();
-
   }
-
+  // if Enter is press and pin is falsy create the pin, otherwise verify it.
   if(e.keyCode == 13) {
-
-    createPin();
-
-  if(pin != null) {
-  replaceBtn();
-  removeButtons();
-  populateButtons();
-  clearScreen();
-  screen.placeholder = 'Enter Your Pin'
+    !pin ? createPin() : verifyPin();
   }
-  }
-  if (screen.value == pin) {
-    verifyPin();
-  }
+  // if entered value length is 4, not null and not equal to the pin,
+  // then verify the pin.
   else if (screen.value.length == 4 && screen.value != pin && pin != null) {
-    screen.value = incorrect
+    verifyPin();
   }
 };
 
